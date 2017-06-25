ccba3ecb8be81bfba968d604e56cdfad9e4c75c7
Loïc Dauphin <astralien3000@yahoo.fr>
2017-06-25 22:15:33 +0200

    moved code

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ extern "C" {
 #include "extApi.h"
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
   simxInt client_id = simxStart("127.0.0.1", 3000, true, true, 1000, 10);
   if(client_id == -1) {
@@ -21,43 +21,23 @@ int main(int argc, char *argv[])
   simxInt left_encoder_handle = -1;
   simxInt right_encoder_handle = -1;
 
-  {
-    simxInt jcount = 0;
-    simxInt* jhandles = NULL;
-    simxGetObjects(client_id, sim_object_joint_type, &jcount, &jhandles, simx_opmode_oneshot_wait);
-
-    simxInt ncount = 0;
-    simxChar* names = NULL;
-    simxGetObjectGroupData(client_id, sim_object_joint_type, 0, &jcount, &jhandles, NULL, NULL, NULL, NULL, &ncount, &names, simx_opmode_oneshot_wait);
-
-    {
-      char* cur = names;
-      for(int i = 0 ; i < ncount ; i++) {
-        if(strcmp("left_motor_joint", cur) == 0) {
-          left_motor_handle = jhandles[i];
-        }
-        if(strcmp("right_motor_joint", cur) == 0) {
-          right_motor_handle = jhandles[i];
-        }
-        if(strcmp("left_encoder_joint", cur) == 0) {
-          left_encoder_handle = jhandles[i];
-        }
-        if(strcmp("right_encoder_joint", cur) == 0) {
-          right_encoder_handle = jhandles[i];
-        }
-        //cout << jhandles[i] << " : " << cur << endl;
-        cur += strlen(cur)+1;
-      }
-    }
-  }
-
-  if(left_motor_handle == -1 || right_motor_handle == -1) {
-    cerr << "ERROR : Could not find motors" << endl;
+  if(simxGetObjectHandle(client_id, "left_motor_joint", &left_motor_handle, simx_opmode_oneshot_wait) != simx_return_ok) {
+    cerr << "ERROR : Could not find left_motor_joint" << endl;
     return -1;
   }
 
-  if(left_encoder_handle == -1 || right_encoder_handle == -1) {
-    cerr << "ERROR : Could not find motors" << endl;
+  if(simxGetObjectHandle(client_id, "right_motor_joint", &right_motor_handle, simx_opmode_oneshot_wait) != simx_return_ok) {
+    cerr << "ERROR : Could not find right_motor_joint" << endl;
+    return -1;
+  }
+
+  if(simxGetObjectHandle(client_id, "left_encoder_joint", &left_encoder_handle, simx_opmode_oneshot_wait) != simx_return_ok) {
+    cerr << "ERROR : Could not find left_encoder_joint" << endl;
+    return -1;
+  }
+
+  if(simxGetObjectHandle(client_id, "right_encoder_joint", &right_encoder_handle, simx_opmode_oneshot_wait) != simx_return_ok) {
+    cerr << "ERROR : Could not find right_encoder_joint" << endl;
     return -1;
   }
 
--- src/vrep_test.pro
+++ src/vrep_test.pro
@@ -5,23 +5,26 @@ CONFIG -= qt
 
 DEFINES += NON_MATLAB_PARSING
 DEFINES += MAX_EXT_API_CONNECTIONS=255
+DEFINES += QT_COMPIL
 
 LIBS += -lWs2_32
 LIBS += -lWinmm
 
+INCLUDEPATH += v_rep_api/include
+
 SOURCES += main.cpp \
-    extApi.c \
-    extApiCustom.c \
-    extApiPlatform.c
+    v_rep_api/extApi.c \
+    v_rep_api/extApiCustom.c \
+    v_rep_api/extApiPlatform.c
 
 HEADERS += \
-    extApi.h \
-    extApiCustom.h \
-    extApiInternal.h \
-    extApiPlatform.h \
-    v_repConst.h \
-    extApiCustomConst.h \
-    socketInConnection.h \
-    socketOutConnection.h \
-    v_repLib.h \
-    v_repTypes.h
+    v_rep_api/include/extApi.h \
+    v_rep_api/include/extApiCustom.h \
+    v_rep_api/include/extApiCustomConst.h \
+    v_rep_api/include/extApiInternal.h \
+    v_rep_api/include/extApiPlatform.h \
+    v_rep_api/include/socketInConnection.h \
+    v_rep_api/include/socketOutConnection.h \
+    v_rep_api/include/v_repConst.h \
+    v_rep_api/include/v_repLib.h \
+    v_rep_api/include/v_repTypes.h
